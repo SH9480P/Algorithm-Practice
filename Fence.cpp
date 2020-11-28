75e7bfefd269f1bca67ca3712c82639467f8ebd9
ddophi98 <72330884+ddophi98@users.noreply.github.com>
2020-11-28 21:43:11 +0900

    울타리 잘라내기 (195p)

---
+++ Fence.cpp
@@ -0,0 +1,62 @@
+﻿
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int minimumHeight(vector<int> fenceHeight, int num) {
+	
+	int currentHeight;
+	int rightWidth, leftWidth;
+	int maximumSize = -1;
+	for (int i = 0; i < num; i++) {
+		currentHeight = fenceHeight[i];
+		rightWidth = 0;
+		leftWidth = 0;
+
+		for (int j = i; j < num; j++) {
+			if (fenceHeight[j] < currentHeight)
+				break;
+			rightWidth++;
+		}
+
+		for (int j = i; j >= 0; j--) {
+			if (fenceHeight[j] < currentHeight)
+				break;
+			leftWidth++;
+		}
+
+		maximumSize = max(maximumSize, currentHeight * (leftWidth + rightWidth - 1));
+	}
+	return maximumSize;
+}
+
+int main()
+{
+	int C; 
+	int N; 
+	int height;
+	vector<int> fenceHeight;
+	vector<int> answer;
+
+
+	cin >> C;
+	for (int i = 0; i < C; i++) {
+
+		cin >> N;
+		for (int j = 0; j < N; j++) {
+			cin >> height;
+			fenceHeight.push_back(height);
+		}
+		answer.push_back(minimumHeight(fenceHeight, N));
+		fenceHeight.clear();
+	}
+
+	for (int i = 0; i < C; i++) {
+		cout << answer[i] << "\n";
+	}
+
+}
+
+
